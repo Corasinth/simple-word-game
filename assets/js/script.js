13924bc6741c2d54436c12bacc16b84ed1de8461
Corasinth <beforeyouknowkindness@gmail.com>
2022-06-27 15:56:41 -0500

    add pseudocode

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+//Event listener on a button to start a timer/start the game, also resets timer 
+
+//A timer 
+
+//Ability to log keypress. 
+
+//Need a way to store a word (array probably)
+
+//If key press is in a specified word, those letters show up
+
+//Store wins and losses in local storage
+
+//Display wins and losses from local storage
+
+//Game win condition when word is guessed, and message of winning is displayed
+
+//Game loss condition when timer runs out and word is not guessed, and message of losing is displayed 
+
+
